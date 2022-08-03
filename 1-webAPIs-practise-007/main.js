c341c9e0af86358ad099b95b1e4ddf45b85dd242
lani <laniiiiworld@gmail.com>
2022-08-03 20:57:24 +0900

    DOM 완전 정복(좌표 실습 성능 개선)

--- 1-webAPIs-practise-007/main.js
+++ 1-webAPIs-practise-007/main.js
@@ -4,23 +4,21 @@
   const lineY = document.querySelector('.lineY');
   const coordinates = document.querySelector('.coordinates');
 
-  //마우스 이동시 이벤트 함수
-  function mousemove(event) {
-    const x = event.clientX;
-    const y = event.clientY;
+  window.addEventListener('load', () => {
+    const iconRect = icon.getBoundingClientRect();
+    //마우스 이동시 이벤트 함수
+    window.addEventListener('mousemove', (event) => {
+      const x = event.clientX;
+      const y = event.clientY;
 
-    //좌표
-    coordinates.style.left = `${x}px`;
-    coordinates.style.top = `${y}px`;
-    coordinates.innerText = `${x}, ${y}`;
-    //라인
-    lineX.style.top = `${y}px`;
-    lineY.style.left = `${x}px`;
-    //아이콘
-    icon.style.left = `${x}px`;
-    icon.style.top = `${y}px`;
-  }
-
-  window.addEventListener('mousemove', mousemove);
-  //window.addEventListener('load', () => {});
+      //좌표
+      coordinates.style.transform = `translate(${x}px, ${y}px)`;
+      coordinates.innerText = `${x}, ${y}`;
+      //라인
+      lineX.style.transform = `translateY(${y}px)`;
+      lineY.style.transform = `translateX(${x}px)`;
+      //아이콘
+      icon.style.transform = `translate(${x - iconRect.width / 2}px, ${y - iconRect.height / 2}px)`;
+    });
+  });
 }
